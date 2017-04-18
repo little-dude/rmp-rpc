c5e7d4cdbfd554042a99a1d6e157481f59263d12
Corentin Henry <corentinhenry@gmail.com>
2017-04-18 13:39:39 -0700

    calculator example: rename error && cleanup

--- examples/calculator/src/client.rs
+++ examples/calculator/src/client.rs
@@ -17,7 +17,7 @@ pub fn connect(addr: &SocketAddr) -> Result<Client, io::Error> {
     Ok(client)
 }
 
-pub type Response = oneshot::Receiver<Result<i64, CalculatorError>>;
+pub type Response = oneshot::Receiver<Result<i64, RpcError>>;
 
 pub struct Client {
     client: rmp_rpc::Client,
@@ -53,46 +53,34 @@ impl Client {
     }
 }
 
-fn parse_response(response: Result<Result<Value, Value>, io::Error>)
-                  -> Result<i64, CalculatorError> {
+fn parse_response(response: Result<Result<Value, Value>, io::Error>) -> Result<i64, RpcError> {
     match response? {
         Ok(result) => {
             if let Value::Integer(int) = result {
-                match int.as_i64() {
-                    Some(int) => {
-                        return Ok(int);
-                    }
-                    None => {
-                        return Err(CalculatorError::Client(format!("Could not parse server \
-                                                                    response as an integer")));
-                    }
-                }
+                int.as_i64().ok_or(RpcError::Client(format!("Could not parse server response as \
+                                                             an integer")))
             } else {
-                return Err(CalculatorError::Client(format!("Could not parse server response as \
-                                                            an integer")));
+                Err(RpcError::Client(format!("Could not parse server response as an integer")))
             }
         }
         Err(error) => {
             if let Value::String(s) = error {
                 match s.as_str() {
-                    Some(error_str) => {
-                        return Err(CalculatorError::Server(error_str.to_string()));
-                    }
+                    Some(error_str) => Err(RpcError::Server(error_str.to_string())),
                     None => {
-                        return Err(CalculatorError::Client(format!("Could not parse server \
-                                                                    response as a string")));
+                        Err(RpcError::Client(format!("Could not parse server response as a \
+                                                      string")))
                     }
                 }
             } else {
-                return Err(CalculatorError::Client(format!("Could not parse server response as \
-                                                            a string")));
+                Err(RpcError::Client(format!("Could not parse server response as a string")))
             }
         }
     }
 }
 
 #[derive(Debug)]
-pub enum CalculatorError {
+pub enum RpcError {
     /// IO error that occured while communicating with the server.
     Io(io::Error),
     /// Error returned by the server upon a request.
@@ -101,41 +89,41 @@ pub enum CalculatorError {
     Client(String),
 }
 
-impl fmt::Display for CalculatorError {
+impl fmt::Display for RpcError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            CalculatorError::Io(ref err) => {
+            RpcError::Io(ref err) => {
                 write!(f,
                        "an IO error occured while communicating with the server: {}",
                        err)
             }
-            CalculatorError::Server(ref msg) => write!(f, "the server returned an error: {}", msg),
-            CalculatorError::Client(ref msg) => {
+            RpcError::Server(ref msg) => write!(f, "the server returned an error: {}", msg),
+            RpcError::Client(ref msg) => {
                 write!(f, "failed to process the server response (reason: {})", msg)
             }
         }
     }
 }
 
-impl error::Error for CalculatorError {
+impl error::Error for RpcError {
     fn description(&self) -> &str {
         match *self {
-            CalculatorError::Io(_) => "an IO error occured while communicating with the server",
-            CalculatorError::Server(_) => "the server returned an error",
-            CalculatorError::Client(_) => "failed to process the server response",
+            RpcError::Io(_) => "an IO error occured while communicating with the server",
+            RpcError::Server(_) => "the server returned an error",
+            RpcError::Client(_) => "failed to process the server response",
         }
     }
 
     fn cause(&self) -> Option<&error::Error> {
         match *self {
-            CalculatorError::Io(ref e) => Some(e),
+            RpcError::Io(ref e) => Some(e),
             _ => None,
         }
     }
 }
 
-impl From<io::Error> for CalculatorError {
-    fn from(err: io::Error) -> CalculatorError {
-        CalculatorError::Io(err)
+impl From<io::Error> for RpcError {
+    fn from(err: io::Error) -> RpcError {
+        RpcError::Io(err)
     }
 }
