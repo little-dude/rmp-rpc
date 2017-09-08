950cd8dff36c9cda44d9e183b7009520cbffceee
Corentin Henry <corentinhenry@gmail.com>
2017-09-07 21:59:24 -0700

    cleanup ping pong example

--- examples/ping_pong.rs
+++ examples/ping_pong.rs
@@ -1,17 +1,20 @@
 extern crate env_logger;
 extern crate futures;
+#[macro_use]
+extern crate log;
 extern crate rmp_rpc;
 extern crate tokio_core;
 
 use std::{io, thread};
-use std::time::Duration;
 use std::net::SocketAddr;
-
-use rmp_rpc::{serve, Client, Connector, Service, ServiceBuilder, Value};
-use tokio_core::reactor::Core;
 use std::sync::{Arc, Mutex};
+use std::time::Duration;
 
 use futures::{future, Future};
+use tokio_core::reactor::Core;
+
+use rmp_rpc::{serve, Client, Connector, Service, ServiceBuilder, Value};
+
 
 #[derive(Clone)]
 pub struct PingPong {
@@ -36,28 +39,31 @@ impl Service for PingPong {
     fn handle_request(
         &mut self,
         method: &str,
-        _: &[Value],
+        params: &[Value],
     ) -> Box<Future<Item = Result<Self::T, Self::E>, Error = Self::Error>> {
         let client = self.client.clone();
         match method {
             "ping" => {
-                return Box::new(
-                    client
-                        .unwrap()
-                        .request("pong", &[])
-                        .and_then(|_result| Ok(Ok(String::new())))
-                        .map_err(|()| {
-                            io::Error::new(io::ErrorKind::Other, "The pong request failed")
-                        }),
-                )
+                let id = params[0].as_i64().unwrap();
+                debug!("received ping({}), sending pong", id);
+                let request = client
+                    .unwrap()
+                    .request("pong", &[id.into()])
+                    .and_then(|_result| Ok(Ok(String::new())))
+                    .map_err(|()| {
+                        io::Error::new(io::ErrorKind::Other, "The pong request failed")
+                    });
+                Box::new(request)
             }
             "pong" => {
+                let id = params[0].as_i64().unwrap();
+                debug!("received pong({}), incrementing pong counter", id);
                 *self.value.lock().unwrap() += 1;
-                return Box::new(future::ok(Ok(String::new())));
+                Box::new(future::ok(Ok(String::new())))
             }
             method => {
                 let err = Err(format!("Invalid method {}", method));
-                return Box::new(future::ok(err));
+                Box::new(future::ok(err))
             }
         }
     }
@@ -84,9 +90,10 @@ impl ServiceBuilder for PingPong {
 
 fn main() {
     env_logger::init().unwrap();
+
     thread::spawn(|| {
         let addr: SocketAddr = "127.0.0.1:54321".parse().unwrap();
-        serve(&addr.clone(), PingPong::new())
+        serve(&addr, PingPong::new())
     });
     thread::sleep(Duration::from_millis(100));
 
@@ -99,12 +106,20 @@ fn main() {
             .set_service_builder(ping_pong_client.clone())
             .connect()
             .or_else(|e| {
-                println!("Connection to server failed: {}", e);
+                error!("Connection to server failed: {}", e);
                 Err(())
             })
             .and_then(|client| {
-                client.request("ping", &[]).and_then(|_response| Ok(()))
+                let mut requests = vec![];
+                for i in 0..10 {
+                    requests.push(
+                        client
+                            .request("ping", &[i.into()])
+                            .and_then(|_response| Ok(())),
+                    );
+                }
+                future::join_all(requests)
             }),
     ).unwrap();
-    println!("{}", ping_pong_client.value.lock().unwrap());
+    info!("Received {} pongs", ping_pong_client.value.lock().unwrap());
 }
