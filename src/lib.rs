3a789be751dbdd894723362eb0429902de0cfd36
Corentin Henry <corentinhenry@gmail.com>
2017-09-09 10:25:56 -0700

    run clippy with --allow type_complexity

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 #![cfg_attr(feature = "clippy", plugin(clippy))]
 #![cfg_attr(feature = "clippy", deny(clippy))]
 #![cfg_attr(feature = "clippy", allow(missing_docs_in_private_items))]
+#![cfg_attr(feature = "clippy", allow(type_complexity))]
 
 extern crate bytes;
 extern crate futures;
