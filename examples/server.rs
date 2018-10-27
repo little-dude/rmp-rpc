4b7c19c9e62987c532b02eba7414e6bf9e62a73d
Corentin Henry <corentinhenry@gmail.com>
2018-10-27 12:24:05 -0700

    update server example
    The server example now highlights the crate's async features.
    
    Adapted from @Riamse's code in
    https://github.com/little-dude/rmp-rpc/issues/19

--- examples/server.rs
+++ examples/server.rs
@@ -1,70 +1,111 @@
-//! Here is an simple example with a pure server. A "pure" server cannot send requests or
-//! notifications, and only handles incoming requests and notifications. `rmp-rpc` makes it
-//! possible to have a server that also act as a client and is able to send requests and
-//! notifications to the remote endpoint.
+//! Here is an simple example with a pure server that that provides a method ``do_long_computation`
+//! that simulates a long computation. This server can be tested with the following python script
+//! (which requires [`msgpack-rpc-python`](https://github.com/msgpack-rpc/msgpack-rpc-python):
+//!
+//! ```python
+//! import time
+//! import msgpackrpc
+//!
+//! client = client = msgpackrpc.Client(msgpackrpc.Address("127.0.0.1", 54321))
+//! start = time.time()
+//! requests = []
+//! for i in range(0, 1000):
+//!     requests.append(client.call_async('do_long_computation', 5))
+//! for req in requests:
+//!     req.get()
+//! end = time.time()
+//! print(end - start)
+//! ```
+//#![feature(futures_api)]
+extern crate chrono;
+extern crate env_logger;
 extern crate futures;
 extern crate rmp_rpc;
 extern crate tokio;
-
+#[macro_use]
+extern crate log;
 use std::net::SocketAddr;
+use std::time;
 
-use futures::{Future, Stream};
+use futures::{future, Future, Poll, Stream};
 use rmp_rpc::{serve, Service, Value};
 use tokio::net::TcpListener;
+use tokio::timer::Delay;
 
-// Our server type
+/// Our server type
 #[derive(Clone)]
-pub struct Echo;
+pub struct Server;
 
-// The Service trait defines how the server handles incoming requests and notifications.
-impl Service for Echo {
-    // This is the type of future we send back from `handle_request`. Since we have a response
-    // available immediately, there's no need to send back a "genuine" future.
-    type RequestFuture = Result<Value, Value>;
+/// A future that does nothing but simulates a long computation and returns the time at which is
+/// finishes in seconds.
+pub struct LongComputation(Delay);
 
-    // Define how the server handle requests.
-    //
-    // This server accept requests with the method "echo".
-    // It echoes back the first parameter.
-    // If the method is not echo, or if the first parameter is not a string, it returns an error.
+impl Future for LongComputation {
+    type Item = Value;
+    type Error = Value;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        info!("polling LongComputation");
+        self.0
+            .poll()
+            .map(|future| {
+                future.map(|()| {
+                    time::SystemTime::now()
+                        .duration_since(time::UNIX_EPOCH)
+                        .unwrap()
+                        .as_secs()
+                        .into()
+                })
+            })
+            .map_err(|_| "error".into())
+    }
+}
+
+/// The Service trait defines how the server handles incoming requests and notifications.
+impl Service for Server {
+    type RequestFuture = Box<Future<Item = Value, Error = Value> + Send>;
+
+    /// Define how the server handle requests. This server accept requests with the method
+    /// "do_long_computation" and an integer as parameter. It waits for the number of seconds specified in the parameter, and then sends back the server's time in seconds.
     fn handle_request(&mut self, method: &str, params: &[Value]) -> Self::RequestFuture {
-        // If the method is not "echo", return an error. Note that we "return" by sending back a
-        // message on the return channel. If we wanted to, we could spawn some long-running
-        // computation on another thread, and have that computation be in charge of sending back
-        // the result.
-        if method != "echo" {
-            return Err(format!("Unknown method {}", method).into());
+        if method != "do_long_computation" {
+            return Box::new(future::err(format!("Invalid method {}", method).into()));
         }
-
-        // Take the first parameter, which should be a string, and echo it back
-        if let Value::String(ref string) = params[0] {
-            if let Some(text) = string.as_str() {
-                return Ok(text.into());
+        if params.len() != 1 {
+            return Box::new(future::err(
+                "'do_long_computation' takes one argument".into(),
+            ));
+        }
+        if let Value::Integer(ref value) = params[0] {
+            if let Some(value) = value.as_u64() {
+                return Box::new(LongComputation(Delay::new(
+                    time::Instant::now() + time::Duration::from_secs(value),
+                )));
             }
         }
-
-        // If we reach this point, return an error, that means the first parameter is not a String.
-        Err("Invalid argument".into())
+        Box::new(future::err("Argument must be an unsigned integer".into()))
     }
 
-    // Define how the server handle notifications.
-    //
-    // This server just prints the method in the console.
+    /// Define how the server handle notifications. This server just prints the method in the
+    /// console.
     fn handle_notification(&mut self, method: &str, _: &[Value]) {
         println!("{}", method);
     }
 }
 
 fn main() {
+    env_logger::init();
     let addr: SocketAddr = "127.0.0.1:54321".parse().unwrap();
     // Create a listener to listen for incoming TCP connections.
     let server = TcpListener::bind(&addr)
         .unwrap()
         .incoming()
         // Each time the listener finds a new connection, start up a server to handle it.
-        .map_err(|e| println!("error on TcpListener: {}", e))
+        .map_err(|e| info!("error on TcpListener: {}", e))
         .for_each(move |stream| {
-            serve(stream, Echo).map_err(|e| println!("server error {}", e))
+            info!("new connection {:?}", stream);
+            info!("spawning a new Server");
+            serve(stream, Server).map_err(|e| info!("server error {}", e))
         });
 
     // Run the server on the tokio event loop. This is blocking. Press ^C to stop
